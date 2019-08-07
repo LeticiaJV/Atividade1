3d355deba2acf8de3e00bd39cc7dcdb5c36f1ce8
LeticiaJV <53631364+LeticiaJV@users.noreply.github.com>
2019-08-07 20:05:09 -0300

    Add files via upload

---
+++ Projeto1_Tabuada/src/projeto1_tabuada/Projeto1_Tabuada.java
@@ -0,0 +1,39 @@
+package projeto1_tabuada;
+
+import java.util.Scanner;
+
+public class Projeto1_Tabuada {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+	int numero;
+        boolean validacao;
+        char opcao;
+	do
+	{
+	    try
+	    {
+                System.out.println("Selecione um número para que sua tabuada seja exibida: \n");
+    	        numero = entrada.nextInt();
+    		for (int n = 1; n <= 10; n++)
+    		{
+    		    System.out.println(n + " x " + numero + " = " + (n * numero));
+    		}
+    		System.out.println("Resultado obtido. Deseja informar outro número <S/N>?\n");
+    		opcao = entrada.next().charAt(0);
+                
+                if (opcao == 'S' || opcao == 's')
+                    validacao = true;
+                
+                else
+                    validacao = false;                
+	    }
+	    catch (Exception erro)
+            {                
+	        System.out.println("Entrada incorreta");
+                return;
+	    }
+	}
+	while (validacao == true);
+    }   
+}
